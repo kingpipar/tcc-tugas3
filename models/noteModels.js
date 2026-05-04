2ca9a3241b5f9e3b9e4ac69526df2e5ab4d59845
Al Faarray <150516491+kingpipar@users.noreply.github.com>
2026-05-04 22:45:00 +0700

    fix notemodels.js

--- models/noteModels.js
+++ models/noteModels.js
@@ -1,39 +1,39 @@
 const Note = require('../schema/Note.js');
 
-const getAllNotes = async (req, res) => {
-    try {
-        const notes = await Note.getNotes();
-        res.json(notes);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+const getNotes = async () => { 
+  return await Note.findAll({ 
+    attributes: ['id', 'judul', 'isi', 'tanggal_dibuat'],
+    order: [['tanggal_dibuat', 'DESC']]
+  });
 }
 
-const createNewNote = async (req, res) => {
-    try {
-        const newNote = await Note.createNote(req.body);
-        res.status(201).json(newNote);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+const createNote = async (data) => { 
+  return await Note.create(data);
 }
 
-const updateExistingNote = async (req, res) => {
-    try {
-        await Note.updateNote(req.params.id, req.body);
-        res.json({ message: "Catatan berhasil diupdate" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+// const findById = async (id) => {
+//   return await Note.findByPk(id, {
+//     attributes: ["id", "judul", "isi", "tanggal_dibuat"],
+//   });
+// }
 
-const deleteExistingNote = async (req, res) => {
-    try {
-        await Note.deleteNote(req.params.id);
-        res.json({ message: "Catatan berhasil dihapus" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+const updateNote = async (id, data) => {
+  return await Note.update(data, { 
+    where: {
+      id: id, 
+    }, 
+  });
 }
-
-module.exports = { getAllNotes, createNewNote, updateExistingNote, deleteExistingNote };
+const deleteNote = async (id) =>{ 
+  return await Note.destroy({ 
+    where: {
+      id: id, 
+    }, 
+  });
+}
+module.exports = { 
+  getNotes,
+  createNote,
+  updateNote, 
+  deleteNote 
+};

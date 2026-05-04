bd8e9c53161c000ec6298201a9f0dca60f582a39
Al Faarray <150516491+kingpipar@users.noreply.github.com>
2026-05-04 21:34:12 +0700

    balik ke port 3000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const noteRoutes = require('./routes/noteRoutes.js');
 const NoteSchema = require('./schema/Note.js');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());

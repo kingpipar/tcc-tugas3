272efeca79b0466c8c26596309e6ce38ab50c35e
kingpipar <masfaremmm@gmail.com>
2026-05-04 19:12:28 +0700

    be

---
+++ routes/noteRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { getAllNotes, createNewNote, updateExistingNote, deleteExistingNote } = require('../controllers/noteController.js');
+
+router.get('/notes', getAllNotes);
+router.post('/notes', createNewNote);
+router.put('/notes/:id', updateExistingNote);
+router.delete('/notes/:id', deleteExistingNote);
+
+module.exports = router;
